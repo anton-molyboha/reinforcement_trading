d446b3102c5ec57d1e7452ed6f487604bfdcd869
anton <anton.molyboha@gmail.com>
2018-05-05 12:57:37 -0400

    reinforcement_example.learner.MCLearner: Monte-Carlo learner

--- src/reinforcement_example/learner.py
+++ src/reinforcement_example/learner.py
@@ -1,5 +1,6 @@
 import numpy as np
 import keras
+from reinforcement_trading.tools import logodds_to_probs, weights_to_inds
 
 
 def make_model(inputs, outputs):
@@ -11,6 +12,50 @@ def make_model(inputs, outputs):
     #model.compile('sgd', loss='mean_squared_error')
     model.compile('RMSProp', loss='mean_squared_error')
     return model
+
+
+def make_model_rlu(inputs, outputs):
+    input_layer = keras.layers.Input(shape=(inputs,))
+    layer1 = keras.layers.Dense((inputs + outputs)**2, activation='elu')
+    layer2 = keras.layers.Dense(outputs, activation='elu')
+    scale_layer = keras.layers.Dense(outputs, activation='linear')
+    model = keras.Model(inputs=input_layer, outputs=scale_layer(layer2(layer1(input_layer))))
+    #model.compile('sgd', loss='mean_squared_error')
+    model.compile('RMSProp', loss='mean_squared_error')
+    return model
+
+
+class ScaledModel(object):
+    def __init__(self, model, xscale=None, yscale=None):
+        self.model = model
+        self.xscale = xscale
+        self.yscale = yscale
+
+    def fit(self, X, Y):
+        rX = X / self.xscale[np.newaxis, :] if self.xscale is not None else X
+        rY = Y / self.yscale if self.yscale is not None else Y
+        self.model.fit(rX, rY)
+
+    def predict(self, X):
+        rX = X / self.xscale[np.newaxis, :] if self.xscale is not None else X
+        res = self.model.predict(rX)
+        return res * self.yscale if self.yscale is not None else res
+
+
+class ValueToPolicy(object):
+    def __init__(self, value_model, scale=1.0):
+        self.value_model = value_model
+        self.num_actions = len(value_model)
+        self.scale = scale
+
+    def predict(self, states):
+        res = np.array([model.predict(states)[:, 0] for model in self.value_model]) / self.scale
+        # res has axes: action, batch
+        # Result must have axes: batch, action
+        return res.T
+
+    def set_scale(self, scale):
+        self.scale = scale
 
 
 class AbstractLearner(object):
@@ -68,6 +113,72 @@ class SimpleLearner(AbstractLearner):
     def move(self, state):
         move, score = self._move_and_score(state)
         return move
+
+
+class MCLearner(AbstractLearner):
+    def __init__(self, state_dim, num_actions, reward_scale=1.0, model_factory=make_model_rlu):
+        self.state_dim = state_dim
+        self.num_actions = num_actions
+        self.reward_scale = reward_scale
+        self.value_model = [ScaledModel(model_factory(state_dim, 1), yscale=reward_scale) for i in range(num_actions)]
+        #self.policy = make_model(state_dim, num_actions)
+        self.policy = ValueToPolicy(self.value_model, scale=self.reward_scale)
+        self.histories = []
+        self.history = []
+        # Helper arrays for faster training
+        self.states = np.empty((0, self.state_dim))
+        self.actions = np.empty(0, dtype=np.int)
+        self.cumrewards = np.empty(0)
+        self.cumlogprobs = np.empty(0)
+        self.ranges = np.empty((0, 2), dtype=np.int)
+        self.actinds = [np.empty(0, dtype=np.int) for i in range(self.num_actions)]
+
+    def move(self, state):
+        probs = logodds_to_probs(self.policy.predict(state[np.newaxis, :])[0])
+        choice = np.searchsorted(np.cumsum(probs), np.random.rand())
+        self.last_action_prob = probs[choice]
+        return choice
+
+    def learn(self, state, action, reward, next_state, value_proxy):
+        self.history.append((np.array(state), action, reward, self.last_action_prob))
+
+    def learn_last(self, state, action, reward):
+        self.history.append((np.array(state), action, reward, self.last_action_prob))
+        self.histories.append(self.history)
+        self.history = []
+        self._update_value_model()
+
+    def _update_value_model(self):
+        # Update data structures
+        last_states, last_actions, last_rewards, last_actionprob = zip(*self.histories[-1][::-1])
+        dsize = len(self.states)
+        self.ranges = np.concatenate([self.ranges, np.array([[dsize, dsize + len(last_states)]])])
+        dsize += len(last_states)
+        self.states = np.concatenate([self.states, np.array(last_states)], axis=0)
+        self.actions = np.concatenate([self.actions, np.array(last_actions, dtype=np.int)])
+        self.cumrewards = np.concatenate([self.cumrewards, np.cumsum(last_rewards)])
+        self.cumlogprobs = np.concatenate([self.cumlogprobs, [0], np.cumsum(np.log(last_actionprob))[:-1]])
+        self.actinds = [np.arange(len(self.actions))[self.actions == i] for i in range(self.num_actions)]
+        # Training
+        policy_logprobs = np.log(logodds_to_probs(self.policy.predict(self.states))[np.arange(dsize), self.actions])
+        policy_cumlogprobs = np.zeros(dsize)
+        for start, end in self.ranges:
+            policy_cumlogprobs[start + 1:end] = np.cumsum(policy_logprobs[start:end - 1])
+        predicted_value = np.zeros(dsize)
+        for action in range(self.num_actions):
+            if len(self.actinds[action]) > 0:
+                predicted_value[self.actinds[action]] = \
+                    self.value_model[action].predict(self.states[self.actinds[action], :]).reshape(-1)
+        weights = np.exp(policy_cumlogprobs - self.cumlogprobs)
+        assert np.all(weights >= 0)
+        for action in range(self.num_actions):
+            inds = self.actinds[action][weights_to_inds(weights[self.actinds[action]])]
+            if len(inds) > 0:
+                self.value_model[action].fit(self.states[inds, :], self.cumrewards[inds])
+        # Policy scale
+        mse = np.dot(weights, (predicted_value - self.cumrewards) ** 2) / np.sum(weights)
+        self.policy.set_scale(self.reward_scale / len(self.histories) + np.sqrt(mse))
+        return
 
 
 # Play the game
--- src/reinforcement_trading/tools.py
+++ src/reinforcement_trading/tools.py
@@ -7,3 +7,25 @@ def indexed_concat(lst):
     for i, datum in enumerate(adjusted):
         datum.index = pd.MultiIndex.from_product([[i], datum.index])
     return pd.concat(adjusted, axis=0)
+
+
+def random_round(a):
+    shape = a.shape if hasattr(a, 'shape') else (1,)
+    flr = np.floor(a).astype(np.int64, copy=False)
+    return flr + (np.random.rand(*shape) < a - flr)
+
+
+def logodds_to_probs(odds):
+    scaled = np.asarray(odds) - np.expand_dims(np.max(odds, axis=-1), axis=-1)
+    transformed = np.exp(scaled)
+    return transformed / np.expand_dims(np.sum(transformed, axis=-1), axis=-1)
+
+
+def weights_to_inds(weights, amplification=10):
+    copy_counts = random_round(weights * amplification)
+    res = np.empty(np.sum(copy_counts), dtype=np.int64)
+    ptr = 0
+    for i, cnt in enumerate(copy_counts):
+        res[ptr:ptr+cnt] = i
+        ptr += cnt
+    return res
